f33ecf8f64e2a4f813b77295a637d7632749ea78
John Kojo Ampia - Addison <jojoaddison@gmail.com>
2016-06-28 09:16:34 +0200

    Dashboard

--- src/main/webapp/app/blocks/config/http.config.js
+++ src/main/webapp/app/blocks/config/http.config.js
@@ -5,14 +5,14 @@
         .module('agreenApp')
         .config(httpConfig);
 
-    httpConfig.$inject = ['$urlRouterProvider', '$httpProvider', 'httpRequestInterceptorCacheBusterProvider', '$urlMatcherFactoryProvider'];
+    httpConfig.$inject = ['$urlRouterProvider', '$httpProvider', 'httpRequestInterceptorCacheBusterProvider', '$urlMatcherFactoryProvider', 'SERVER'];
 
-    function httpConfig($urlRouterProvider, $httpProvider, httpRequestInterceptorCacheBusterProvider, $urlMatcherFactoryProvider) {
-        
+    function httpConfig($urlRouterProvider, $httpProvider, httpRequestInterceptorCacheBusterProvider, $urlMatcherFactoryProvider, SERVER) {
+
         //enable CSRF
         $httpProvider.defaults.xsrfCookieName = 'CSRF-TOKEN';
         $httpProvider.defaults.xsrfHeaderName = 'X-CSRF-TOKEN';
-        
+
         //Cache everything except rest api requests
         httpRequestInterceptorCacheBusterProvider.setMatchlist([/.*api.*/, /.*protected.*/], true);
 
@@ -21,6 +21,22 @@
         $httpProvider.interceptors.push('errorHandlerInterceptor');
         $httpProvider.interceptors.push('authExpiredInterceptor');
         $httpProvider.interceptors.push('notificationInterceptor');
+        $httpProvider.interceptors.push(function($q){
+
+            return{
+                'request' : function(config){
+
+                    //console.log(window.location);
+                    //console.log(config);
+
+                    if(window.location.hostname.indexOf('localhost') < 0  && config.url.indexOf('api') == 0){
+                        config.url = SERVER + config.url;
+                    }
+                    return config || $q.when(config);
+                }
+            }
+
+        });
         // jhipster-needle-angularjs-add-interceptor JHipster will add new application http interceptor here
 
         $urlMatcherFactoryProvider.type('boolean', {
